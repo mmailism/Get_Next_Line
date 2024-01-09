3410ae93de1e05c523291b7ee325aabc876a3d7c
Phanuwat <laernoyats@gmail.com>
2024-01-09 17:21:37 +0700

    fix

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iammai <iammai@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maramick <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:13:01 by kpueankl          #+#    #+#             */
-/*   Updated: 2024/01/08 17:51:53 by iammai           ###   ########.fr       */
+/*   Updated: 2024/01/09 17:17:01 by maramick         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
 	int		j;
@@ -22,9 +22,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	temp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	temp = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!temp)
+	{
+		free(s1);
 		return (NULL);
+	}
 	while (s1[j])
 		temp[i++] = s1[j++];
 	j = 0;
@@ -56,24 +59,45 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	tmp[j] = '\0';
 	return (tmp);
 }
-char	*ft_strdup(const char *s1)
-{
-	char	*alt;
-	char	n;
-	int		i;
 
-	if (!s1)
-		return (NULL);
-	n = ft_strlen(s1) + 1;
-	alt = (char *)malloc(sizeof(char) * n);
-	if (!alt)
-		return (NULL);
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	s_len;
+	size_t	i;
+
 	i = 0;
-	while (i < n)
+	s_len = ft_strlen(s);
+	dup = (char *)malloc((s_len * sizeof(char)) + 1);
+	if (dup == NULL)
+		return (NULL);
+	while (s[i] != '\0')
 	{
-		alt[i] = s1[i];
+		dup[i] = s[i];
 		i++;
 	}
-	alt[i] = 0;
-	return (alt);
+	dup[i] = '\0';
+	return (dup);
 }
+
+// char	*ft_strdup(const char *s1)
+// {
+// 	char	*alt;
+// 	char	n;
+// 	int		i;
+
+// 	if (!s1)
+// 		return (NULL);
+// 	n = ft_strlen(s1) + 1;
+// 	alt = (char *)malloc(sizeof(char) * n);
+// 	if (!alt)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < n)
+// 	{
+// 		alt[i] = s1[i];
+// 		i++;
+// 	}
+// 	alt[i] = 0;
+// 	return (alt);
+// }
