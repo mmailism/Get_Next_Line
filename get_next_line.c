6a97d8f11fa8c97caa26b8e373c2bd2faf8951bf
Mai <Mai@m-2.local>
2024-01-25 16:48:18 +0700

    gml_fn

--- get_next_line.c
+++ get_next_line.c
@@ -3,25 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maramick <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: Mai <Mai@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:13:01 by kpueankl          #+#    #+#             */
-/*   Updated: 2024/01/25 14:30:07 by maramick         ###   ########.fr       */
+/*   Updated: 2024/01/25 16:45:09 by Mai              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	set_zero(char *ptr, int size)
+char	*set_zero(char *ptr, size_t mem)
 {
 	int	i;
+	int	size;
 
+	size = BUFFER_SIZE + 1;
 	i = 0;
-	while (i != size)
+	if (mem == 1)
 	{
-		ptr[i] = 0;
-		i++;
+		while (i != size)
+		{
+			ptr[i] = 0;
+			i++;
+		}
 	}
+	else if (mem == 2)
+	{
+		if (ptr != NULL)
+			free(ptr);
+		ptr = NULL;
+	}
+	return (NULL);
 }
 
 size_t	ft_strlen(char *str, size_t mem)
@@ -57,49 +69,33 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
-		if (list)
-			free(list);
-		list = NULL;
+		list = set_zero(list, 2);
 		return (NULL);
 	}
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 	{
-		if (list)
-			free(list);
-		list = NULL;
+		list = set_zero(list, 2);
 		return (NULL);
 	}
-	set_zero(buffer, BUFFER_SIZE + 1);
-	line = read_line(fd, buffer, &list);
+	line = read_line(fd, buffer, &list, 1);
 	free(buffer);
 	if (!line)
-	{
-		if (list)
-			free(list);
-		list = NULL;
-		return ( NULL);
-	}
+		return (set_zero(list, 2));
 	list = get_list(line);
 	new_line = get_newline(line);
 	if (!new_line)
-	{
-		if (list)
-			free(list);
-		list = NULL;
-	}
-	free(line);
-	return (new_line);
+		list = set_zero(list, 2);
+	return (free(line), new_line);
 }
 
-char	*read_line(int fd, char *buffer, char **addr_list)
+char	*read_line(int fd, char *buffer, char **addr_list, int rd)
 {
-	int		rd;
 	char	*tmp;
 	char	*list;
 
+	set_zero(buffer, 1);
 	list = *addr_list;
-	rd = 1;
 	while (!(ft_strchr(list, '\n')))
 	{
 		rd = read(fd, buffer, BUFFER_SIZE);
@@ -144,57 +140,3 @@ char	*get_list(char *list)
 	list[i + 1] = '\0';
 	return (tmp);
 }
-
-// char	*get_list(char *list)
-// {
-// 	int		i;
-// 	char	*tmp;
-// 	char	mem;
-
-// 	i = 0;
-// 	mem = ft_strlen(list, 1);
-// 	if (mem == 0)
-// 		return (NULL);
-// 	tmp = (char *)malloc(mem + 1);
-// 	if (!tmp)
-// 		return (NULL);
-// 	while (list[i] != '\0' && list[i] != '\n')
-// 	{
-// 		tmp[i] = list[i];
-// 		i++;
-// 	}
-// 	if (list[i] == '\n')
-// 		tmp[i++] = '\n';
-// 	tmp[i] = '\0';
-// 	tmp = ft_substr(list, i + 1, ft_strlen(list, 1) - 1);
-// 	if (!tmp)
-// 		return (free(tmp), NULL);
-// 	return (tmp);
-// }
-
-// #include <fcntl.h>
-// #include <stdio.h>
-
-// int	main(void)
-// {
-// 	int	fd;
-// 	char	*line;
-
-// 	fd = open("read_error.txt", O_RDONLY);
-
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("%s\n", line);
-// 		free(line);
-// 	}
-// 	close(fd);
-// 	fd = open("43_with_nl.txt", O_RDONLY);
-
-// 	while ((line = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("%s\n", line);
-// 		free(line);
-// 	}
-// 	close(fd);
-// 	return 0;
-// }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maramick <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: Mai <Mai@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:13:01 by kpueankl          #+#    #+#             */
-/*   Updated: 2024/01/25 14:30:25 by maramick         ###   ########.fr       */
+/*   Updated: 2024/01/25 16:46:33 by Mai              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,13 @@
 # endif
 
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <fcntl.h>
 # include <limits.h>
 
-typedef struct s_list
-{
-	char			*content;
-	struct s_list	*next;
-}					t_list;
-
 char	*get_next_line(int fd);
-char	*read_line(int fd, char *buffer, char **addr_list);
+char	*read_line(int fd, char *buffer, char **addr_list, int rd);
 char	*get_list(char *list);
 char	*get_newline(char *data);
 size_t	ft_strlen(char *str, size_t mem);
@@ -39,9 +32,6 @@ char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(char *s);
-// int		found_newline(char *list);
-// char	*count_list(char *list, char *buffer);
-void	set_zero(char *ptr, int size);
-// char	*update_lst(char *list, char *n_line);
+char	*set_zero(char *ptr, size_t mem);
 
 #endif
